130cd33e2f3d4fdc5648e9989a49e74d0ac1d7a4
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-08-09 09:11:29 -0700

    update

--- main.h
+++ main.h
@@ -10,7 +10,6 @@ int print_string(char *string);
 int _strlen(char *s);
 char *chartos(char c);
 char *nothing_found(char c);
-void _puts(char *buffer, int size)
 char *rev_string(char *a, int n);
 
 #endif
